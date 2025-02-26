932f529527c4879cf7b75d1d1ee9054212e6bb56
Adam Gade-Frimann <adg001@edu.zealand.dk>
2025-02-26 14:07:34 +0100

    Fixed comments.

--- CalculatorClassLibrary/Calculator.cs
+++ CalculatorClassLibrary/Calculator.cs
@@ -1,5 +1,5 @@
 ﻿namespace CalculatorClassLibrary
-{
+{   
     public class Calculator
     {
         
---
+++ CalculatorClassLibrary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorClassLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+            Console.WriteLine(calculator.Add(2,3));
+
+            Calculator calculator1 = new Calculator();
+
+            calculator1.Subtract(2,3);
+
+            Console.WriteLine(calculator1.Subtract(2,7);
+        }
+    }
+}
